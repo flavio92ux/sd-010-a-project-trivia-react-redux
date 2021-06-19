7088bb025705938831406630ffca18fe6d450ccc
Flavio Franco <flavio.win@gmail.com>
2021-06-19 11:45:12 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { MD5 } from 'crypto-js';
 
 class Header extends Component {
   render() {
     const { name, email } = this.props;
     return (
-      <div>
-        <p>{name}</p>
-        <p>{email}</p>
-      </div>
+      <header className="navbar navbar-expand-lg navbar-light bg-light">
+        <img
+          data-testid="header-profile-picture"
+          src={ `https://www.gravatar.com/avatar/${MD5(email).toString()}` }
+          alt="profile"
+        />
+        <p data-testid="header-player-name">{name}</p>
+        <p data-testid="header-score">0</p>
+      </header>
     );
   }
 }
