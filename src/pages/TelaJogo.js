2adaf839927211ef54edffe05c373e0a72751de6
Flavio Franco <flavio.win@gmail.com>
2021-06-21 11:33:44 -0300

    implementando redirect

--- src/pages/TelaJogo.js
+++ src/pages/TelaJogo.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import Header from '../components/Header';
 import Questions from '../components/Questions';
 import Timer from '../components/Timer';
 
 class TelaJogo extends Component {
   render() {
+    const { name } = this.props;
     return (
       <div>
+        { !name && <Redirect to="/" /> }
         <Header />
         <Timer />
         <Questions />
@@ -15,4 +20,12 @@ class TelaJogo extends Component {
   }
 }
 
-export default TelaJogo;
+TelaJogo.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  name: state.login.name,
+});
+
+export default connect(mapStateToProps)(TelaJogo);
